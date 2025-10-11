0ec0eefdcf1ce7f5c8d0b7cb5370904e17ddcfd5
Juan Díaz <juandiaz25062000@gmail.com>
2025-10-11 00:28:49 -0600

    Apply .gitignore rules

---
+++ Lesson 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Variables
+    private float speed = 7.0f;
+    private float turnSpeed = 40.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
